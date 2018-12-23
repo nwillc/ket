db31218f309f9e70b71a21b941ef8e9a16003a6b
Nicholas Christopher <nwillc@gmail.com>
2018-12-23 15:33:17 -0500

    Build skeleton

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val assertJVersion = "3.11.1"
+val coverageThreshold = 0.98
+val jacocoToolVersion = "0.8.2"
 val jupiterVersion = "5.3.2"
 val jvmTargetVersion = "1.8"
 
 plugins {
+    jacoco
     kotlin("jvm") version "1.3.11"
     id("com.github.nwillc.vplugin") version "2.3.0"
 }
@@ -25,7 +28,20 @@ dependencies {
     testRuntime("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
 }
 
+jacoco {
+    toolVersion = jacocoToolVersion
+}
+
 tasks {
+    jacocoTestCoverageVerification {
+        violationRules {
+            rule {
+                limit {
+                    minimum = coverageThreshold.toBigDecimal()
+                }
+            }
+        }
+    }
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
